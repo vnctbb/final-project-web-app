7d8a41970c858b837d2c8164757c981c3673c924
Vincent <bongrand.v91@gmail.com>
2021-06-22 23:06:03 +0200

    api local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl : "https://social-med-api.herokuapp.com",
+  //apiBaseUrl : "https://social-med-api.herokuapp.com",
+  apiBaseUrl : "http://192.168.1.11:80",
   staticServerUrl : "https://picture-render-api.herokuapp.com"
 };
